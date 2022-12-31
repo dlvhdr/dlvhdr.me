5f5efc2081d598584caaa1a423cf8c93681efc55
Dolev Hadar <dolevc2@gmail.com>
2022-12-31 15:22:34 +0200

    chore: upgrade to next.js 13
    Signed-off-by: Dolev Hadar <dolevc2@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import { ThemeProvider } from "../context/ThemeContext";
 import "../styles/global.css";
+import localFont from "@next/font/local";
+const monaSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/Mona Sans/Mona-Sans.woff2",
+      weight: "200 700",
+    },
+    {
+      path: "../public/fonts/Mona Sans/Mona-Sans-RegularItalic.woff2",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/Mona Sans/Mona-Sans-BoldItalic.woff2",
+      style: "italic",
+      weight: "bold",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <main className={monaSans.className}>
+        <Component {...pageProps} />
+      </main>
     </ThemeProvider>
   );
 }
